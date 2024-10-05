f84a2c350e67eaee6df2e743553c0a9632c023e3
Rock-it-science <wilmc17@gmail.com>
2024-10-05 16:24:30 -0600

    planetary computer investigate

--- python/n2yo-api.py
+++ python/n2yo-api.py
@@ -4,5 +4,5 @@ import os
 
 load_dotenv()
 
-res = requests.get(f'https://api.n2yo.com/rest/v1/satellite/tle/25544&apiKey={os.environ["N2YO_API_TOKEN"]}')
+res = requests.get(f'https://api.n2yo.com/rest/v1/satellite/tle/39084&apiKey={os.environ["N2YO_API_TOKEN"]}')
 print(res.text)
