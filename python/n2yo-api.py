108b753335ba81d922821aac08e6cc99814b6aa9
Rock-it-science <wilmc17@gmail.com>
2024-10-05 15:23:05 -0600

    init python

---
+++ python/n2yo-api.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+res = requests.get(f'https://api.n2yo.com/rest/v1/satellite/tle/25544&apiKey={os.environ["N2YO_API_TOKEN"]}')
+print(res.text)
